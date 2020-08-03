6f22a58e051e976da9042451a231214a0aa3eaf1
Paloma Mirta Mendez <mpaloma2014@gmail.com>
2020-08-03 19:16:05 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -18,8 +18,9 @@ def remove_strawberry(contacts)
 contacts.each do |person, contact_details_hash|
   if person == "Freddie Mercury"
     contact_details_hash.each do |attribute, data|
-binding.pry
+
 end
+binding.pry
 end
 end
 end
