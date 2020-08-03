ef909688ba31b14a4a1d47c0135216141b8b1586
Paloma Mirta Mendez <mpaloma2014@gmail.com>
2020-08-03 19:19:29 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -19,7 +19,7 @@ contacts.each do |person, contact_details_hash|
   if person == "Freddy Mercury"
     contact_details_hash.each do |attribute, data|
       if attribute == :favorite_ice_cream_flavors
-        date.delete_if {|ice_cream| ice_cream == "strawberry" }
+        data.delete_if {|ice_cream| ice_cream == "strawberry" }
 binding.pry
 end
 end
