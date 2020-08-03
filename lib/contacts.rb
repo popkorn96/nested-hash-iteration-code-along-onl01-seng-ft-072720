dd11087f6f5761ecb59803994a2bbccd27ca62b2
Paloma Mirta Mendez <mpaloma2014@gmail.com>
2020-08-03 19:17:53 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -18,9 +18,10 @@ def remove_strawberry(contacts)
 contacts.each do |person, contact_details_hash|
   if person == "Freddie Mercury"
     contact_details_hash.each do |attribute, data|
-
-end
+      if attribute == :favorite_ice_cream_flavors
+        date.delete_if {|ice_cream| ice_cream == "strawberry" }
 binding.pry
+end
 end
 end
 end
